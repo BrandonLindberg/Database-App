6b38fe73ea0ef7bbeae002d0b2dfe661efa1bec3
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2026-04-02 18:56:00 -0600

    updates

--- public/main.js
+++ public/main.js
@@ -1,13 +1,25 @@
-document.getElementById("view-tables").addEventListener("click", getTables);
+document.getElementById("add-name-button").addEventListener("click", editTable);
+
 const ul = document.getElementById('table-list')
 
-async function getTables() {
-    const request = await fetch('http://database.snailroom.net/tables')
+async function getTable() {
+    const request = await fetch('http://database.snailroom.net/view')
     const tables = await request.json();
-
-    console.log(tables);
 
     tables.forEach(table => {
         ul.innerHTML += `<li>${table.name}</li>`;
     })
 }
+
+async function editTable() {
+    const input = document.getElementById("name-text-box").value;
+
+    const request = await fetch('http://database.snailroom.net/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ input })
+    });
+    getTable();
+}
+
+document.addEventListener('DOMContentLoaded', getTable);
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/tables', async (req, res) => {
+app.get('/view', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM "table_1"');
     res.json(result.rows);
@@ -16,6 +16,18 @@ app.get('/tables', async (req, res) => {
   catch (err) {
     console.error(err);
     res.status(500).send('Database error');
+  }
+});
+
+app.post('/add', async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    const result = await pool.query('INSERT INTO "table_1" (name) VALUES ($1) RETURNING *', [name]);
+    res.json(result.rows[0]);
+  }
+  catch {
+    console.log('oopsies :(');
   }
 });
 
